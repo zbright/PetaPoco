750a8786a01008e992419a654c22d46fec525b6c
Adam Schroder <adamschroder@gmail.com>
2012-02-11 15:23:24 +1100

    Added smart conventions extension which pluralises table names and cleaned up api

--- PetaPoco/PetaPocoFluentMappings.cs
+++ PetaPoco/PetaPocoFluentMappings.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace PetaPoco
 {
@@ -104,7 +106,7 @@ namespace PetaPoco.FluentMappings
             SetFactory(mappings);
         }
 
-        public static void Scan(Action<IPetaPocoConventionScanner> scanner)
+        public static PetaPocoMappings Scan(Action<IPetaPocoConventionScanner> scanner)
         {
             var scannerSettings = new PetaPocoConventionScannerSettings
             {
@@ -112,8 +114,6 @@ namespace PetaPoco.FluentMappings
                 PrimaryKeysNamed = x => "ID",
                 TablesNamed = x => x.Name,
                 PropertiesNamed = x => x.Name,
-                IncludeNameSpaces = x => true,
-                ExcludeTypes = x => false,
                 ResultPropertiesWhere = x => false,
                 VersionPropertiesWhere = x => false
             };
@@ -124,9 +124,9 @@ namespace PetaPoco.FluentMappings
                 scannerSettings.Assemblies.Add(Assembly.GetCallingAssembly());
 
             var types = scannerSettings.Assemblies
-                .SelectMany(y => y.GetExportedTypes().Where(x => scannerSettings.IncludeNameSpaces(x.Namespace)))
-                .Where(x => !x.IsNested && !typeof (PetaPocoMap<>).IsAssignableFrom(x) && !typeof (PetaPocoMappings).IsAssignableFrom(x))
-                .Where(x => !scannerSettings.ExcludeTypes(x));
+                .SelectMany(x => x.GetExportedTypes())
+                .Where(x => scannerSettings.IncludeTypes.All(y => y.Invoke(x)))
+                .Where(x => !x.IsNested && !typeof (PetaPocoMap<>).IsAssignableFrom(x) && !typeof (PetaPocoMappings).IsAssignableFrom(x));
 
             var config = new Dictionary<Type, PetaPocoTypeDefinition>();
 
@@ -154,8 +154,10 @@ namespace PetaPoco.FluentMappings
             }
 
             MergeOverrides(config, scannerSettings.MappingOverrides);
-            
-            SetFactory(new PetaPocoMappings { Config = config });
+
+            var petaPocoMappings = new PetaPocoMappings {Config = config};
+            SetFactory(petaPocoMappings);
+            return petaPocoMappings;
         }
 
         private static void MergeOverrides(Dictionary<Type, PetaPocoTypeDefinition> config, PetaPocoMappings overrideMappings)
@@ -219,14 +221,14 @@ namespace PetaPoco.FluentMappings
         {
             Assemblies = new HashSet<Assembly>();
             IgnorePropertiesWhere = new List<Func<PropertyInfo, bool>>();
+            IncludeTypes = new List<Func<Type, bool>>();
         }
 
         public PetaPocoMappings MappingOverrides { get; set; }
 
         public HashSet<Assembly> Assemblies { get; set; }
         public bool TheCallingAssembly { get; set; }
-        public Func<string, bool> IncludeNameSpaces { get; set; }
-        public Func<Type, bool> ExcludeTypes { get; set; }
+        public List<Func<Type, bool>> IncludeTypes { get; set; }
 
         public Func<Type, string> TablesNamed { get; set; }
         public Func<Type, string> PrimaryKeysNamed { get; set; }
@@ -245,8 +247,7 @@ namespace PetaPoco.FluentMappings
 
         void Assembly(Assembly assembly);
         void TheCallingAssembly();
-        void IncludeNameSpaces(Func<string, bool> nameSpaceFunc);
-        void ExcludeTypes(Func<Type, bool> excludeTypes);
+        void IncludeTypes(Func<Type, bool> includeTypes);
 
         void TablesNamed(Func<Type, string> tableFunc);
         void PrimaryKeysNamed(Func<Type, string> primaryKeyFunc);
@@ -285,14 +286,9 @@ namespace PetaPoco.FluentMappings
             _scannerSettings.TheCallingAssembly = true;
         }
 
-        public void IncludeNameSpaces(Func<string, bool> nameSpaceFunc)
+        public void IncludeTypes(Func<Type, bool> typeIncludes)
         {
-            _scannerSettings.IncludeNameSpaces = nameSpaceFunc;
-        }
-
-        public void ExcludeTypes(Func<Type, bool> excludeTypes)
-        {
-            _scannerSettings.ExcludeTypes = excludeTypes;
+            _scannerSettings.IncludeTypes.Add(typeIncludes);
         }
 
         public void TablesNamed(Func<Type, string> tableFunc)
@@ -324,11 +320,18 @@ namespace PetaPoco.FluentMappings
         IPropertyBuilderConventions VersionWhere(Func<PropertyInfo, bool> versionPropertiesWhereFunc);
     }
 
-    public static class PropertyBuilderExtensions
+    public static class PetaPocoConventionExtensions
     {
         public static IPropertyBuilderConventions IgnoreComplex(this IPropertyBuilderConventions conventions)
         {
             return conventions.IgnoreWhere(y => !(y.PropertyType.IsValueType || y.PropertyType == typeof(string) || y.PropertyType == typeof(byte[])));
+        }
+
+        public static void WithSmartConventions(this IPetaPocoConventionScanner scanner)
+        {
+            scanner.PrimaryKeysNamed(y => y.Name + "Id");
+            scanner.TablesNamed(y => Inflector.MakePlural(y.Name));
+            scanner.Properties.IgnoreComplex();
         }
     }
 
@@ -587,6 +590,217 @@ namespace PetaPoco.FluentMappings
         public bool? ResultColumn { get; set; }
         public bool? IgnoreColumn { get; set; }
         public bool? VersionColumn { get; set; }
+    }
+
+    public static class Inflector
+    {
+        private static readonly List<InflectorRule> _plurals = new List<InflectorRule>();
+        private static readonly List<InflectorRule> _singulars = new List<InflectorRule>();
+        private static readonly List<string> _uncountables = new List<string>();
+
+        static Inflector()
+        {
+            AddPluralRule("$", "s");
+            AddPluralRule("s$", "s");
+            AddPluralRule("(ax|test)is$", "$1es");
+            AddPluralRule("(octop|vir)us$", "$1i");
+            AddPluralRule("(alias|status)$", "$1es");
+            AddPluralRule("(bu)s$", "$1ses");
+            AddPluralRule("(buffal|tomat)o$", "$1oes");
+            AddPluralRule("([ti])um$", "$1a");
+            AddPluralRule("sis$", "ses");
+            AddPluralRule("(?:([^f])fe|([lr])f)$", "$1$2ves");
+            AddPluralRule("(hive)$", "$1s");
+            AddPluralRule("([^aeiouy]|qu)y$", "$1ies");
+            AddPluralRule("(x|ch|ss|sh)$", "$1es");
+            AddPluralRule("(matr|vert|ind)ix|ex$", "$1ices");
+            AddPluralRule("([m|l])ouse$", "$1ice");
+            AddPluralRule("^(ox)$", "$1en");
+            AddPluralRule("(quiz)$", "$1zes");
+
+            AddSingularRule("s$", String.Empty);
+            AddSingularRule("ss$", "ss");
+            AddSingularRule("(n)ews$", "$1ews");
+            AddSingularRule("([ti])a$", "$1um");
+            AddSingularRule("((a)naly|(b)a|(d)iagno|(p)arenthe|(p)rogno|(s)ynop|(t)he)ses$", "$1$2sis");
+            AddSingularRule("(^analy)ses$", "$1sis");
+            AddSingularRule("([^f])ves$", "$1fe");
+            AddSingularRule("(hive)s$", "$1");
+            AddSingularRule("(tive)s$", "$1");
+            AddSingularRule("([lr])ves$", "$1f");
+            AddSingularRule("([^aeiouy]|qu)ies$", "$1y");
+            AddSingularRule("(s)eries$", "$1eries");
+            AddSingularRule("(m)ovies$", "$1ovie");
+            AddSingularRule("(x|ch|ss|sh)es$", "$1");
+            AddSingularRule("([m|l])ice$", "$1ouse");
+            AddSingularRule("(bus)es$", "$1");
+            AddSingularRule("(o)es$", "$1");
+            AddSingularRule("(shoe)s$", "$1");
+            AddSingularRule("(cris|ax|test)es$", "$1is");
+            AddSingularRule("(octop|vir)i$", "$1us");
+            AddSingularRule("(alias|status)$", "$1");
+            AddSingularRule("(alias|status)es$", "$1");
+            AddSingularRule("^(ox)en", "$1");
+            AddSingularRule("(vert|ind)ices$", "$1ex");
+            AddSingularRule("(matr)ices$", "$1ix");
+            AddSingularRule("(quiz)zes$", "$1");
+
+            AddIrregularRule("person", "people");
+            AddIrregularRule("man", "men");
+            AddIrregularRule("child", "children");
+            AddIrregularRule("sex", "sexes");
+            AddIrregularRule("tax", "taxes");
+            AddIrregularRule("move", "moves");
+
+            AddUnknownCountRule("equipment");
+            AddUnknownCountRule("information");
+            AddUnknownCountRule("rice");
+            AddUnknownCountRule("money");
+            AddUnknownCountRule("species");
+            AddUnknownCountRule("series");
+            AddUnknownCountRule("fish");
+            AddUnknownCountRule("sheep");
+        }
+
+        private static void AddIrregularRule(string singular, string plural)
+        {
+            AddPluralRule(String.Concat("(", singular[0], ")", singular.Substring(1), "$"), String.Concat("$1", plural.Substring(1)));
+            AddSingularRule(String.Concat("(", plural[0], ")", plural.Substring(1), "$"), String.Concat("$1", singular.Substring(1)));
+        }
+
+        private static void AddUnknownCountRule(string word)
+        {
+            _uncountables.Add(word.ToLower());
+        }
+
+        private static void AddPluralRule(string rule, string replacement)
+        {
+            _plurals.Add(new InflectorRule(rule, replacement));
+        }
+
+        private static void AddSingularRule(string rule, string replacement)
+        {
+            _singulars.Add(new InflectorRule(rule, replacement));
+        }
+
+        public static string MakePlural(string word)
+        {
+            return ApplyRules(_plurals, word);
+        }
+
+        public static string MakeSingular(string word)
+        {
+            return ApplyRules(_singulars, word);
+        }
+
+        private static string ApplyRules(IList<InflectorRule> rules, string word)
+        {
+            string result = word;
+            if (!_uncountables.Contains(word.ToLower()))
+            {
+                for (int i = rules.Count - 1; i >= 0; i--)
+                {
+                    string currentPass = rules[i].Apply(word);
+                    if (currentPass != null)
+                    {
+                        result = currentPass;
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+
+        public static string ToTitleCase(string word)
+        {
+            return Regex.Replace(ToHumanCase(AddUnderscores(word)), @"\b([a-z])",
+                delegate(Match match) { return match.Captures[0].Value.ToUpper(); });
+        }
+
+        public static string ToHumanCase(string lowercaseAndUnderscoredWord)
+        {
+            return MakeInitialCaps(Regex.Replace(lowercaseAndUnderscoredWord, @"_", " "));
+        }
+
+        public static string AddUnderscores(string pascalCasedWord)
+        {
+            return Regex.Replace(Regex.Replace(Regex.Replace(pascalCasedWord, @"([A-Z]+)([A-Z][a-z])", "$1_$2"), @"([a-z\d])([A-Z])", "$1_$2"), @"[-\s]", "_").ToLower();
+        }
+
+        public static string MakeInitialCaps(string word)
+        {
+            return String.Concat(word.Substring(0, 1).ToUpper(), word.Substring(1).ToLower());
+        }
+
+        public static string MakeInitialLowerCase(string word)
+        {
+            return String.Concat(word.Substring(0, 1).ToLower(), word.Substring(1));
+        }
+
+        public static bool IsStringNumeric(string str)
+        {
+            double result;
+            return (double.TryParse(str, NumberStyles.Float, NumberFormatInfo.CurrentInfo, out result));
+        }
+
+        public static string AddOrdinalSuffix(string number)
+        {
+            if (IsStringNumeric(number))
+            {
+                int n = int.Parse(number);
+                int nMod100 = n % 100;
+
+                if (nMod100 >= 11 && nMod100 <= 13)
+                    return String.Concat(number, "th");
+
+                switch (n % 10)
+                {
+                    case 1:
+                        return String.Concat(number, "st");
+                    case 2:
+                        return String.Concat(number, "nd");
+                    case 3:
+                        return String.Concat(number, "rd");
+                    default:
+                        return String.Concat(number, "th");
+                }
+            }
+            return number;
+        }
+
+        public static string ConvertUnderscoresToDashes(string underscoredWord)
+        {
+            return underscoredWord.Replace('_', '-');
+        }
+
+
+        #region Nested type: InflectorRule
+
+        private class InflectorRule
+        {
+            public readonly Regex regex;
+            public readonly string replacement;
+
+            public InflectorRule(string regexPattern, string replacementText)
+            {
+                regex = new Regex(regexPattern, RegexOptions.IgnoreCase);
+                replacement = replacementText;
+            }
+
+            public string Apply(string word)
+            {
+                if (!regex.IsMatch(word))
+                    return null;
+
+                string replace = regex.Replace(word, replacement);
+                if (word == word.ToUpper())
+                    replace = replace.ToUpper();
+
+                return replace;
+            }
+        }
+
+        #endregion
     }
 
 }
